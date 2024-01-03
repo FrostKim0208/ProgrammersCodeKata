3467e85bd554d249df3764ecead07f333b8d85c4
FrostKim0208 <admiral0208@naver.com>
2024-01-03 10:02:52 +0900

    [Add] CodeKAta 2024.01.03

---
+++ Programmers2024.01.03/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Programmers2024._01._03;
+    using System;
+
+    internal class Program
+    {
+        public string Solution(string my_string, string overwrite_string, int s)
+        {
+        my_string = "Program29b8UYP";
+        overwrite_string = "merS123";
+        s = 7;
+
+            char[] c = overwrite_string.ToCharArray();
+            string answer = my_string.Replace(my_string[s], c[0]);
+            Console.WriteLine(answer);
+            return answer;
+        }
+    }
+    
+
